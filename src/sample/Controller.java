96033cdeb7c8614066797eb17aadc452b302926d
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2019-01-01 00:42:14 +0100

    Interface Vers.01 (with Inti. Constrols )

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField searchText;
+    @FXML
+    private Button searchButton ;
+
+    @FXML
+    public void initialize()
+    {
+        searchButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked()
+        {
+            System.out.println("Result : " + searchText.getText() );
+
+            try {
+                Thread.sleep(10000);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            searchText.clear();
+        }
+
+    @FXML
+    public void noSpace ()
+    {
+        String text = searchText.getText() ;
+        Boolean disableSearchButton = text.isEmpty() || text.trim().isEmpty();
+        searchButton.setDisable(disableSearchButton);
+    }
 }
