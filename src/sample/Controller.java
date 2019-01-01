b297bdeeae2a269ed66909358e624520b90b0923
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2019-01-01 06:26:49 +0100

    Interface Vers.02 (with Inti. Constrols )

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
 
@@ -10,11 +11,16 @@ public class Controller {
     private TextField searchText;
     @FXML
     private Button searchButton ;
+    @FXML
+    private GridPane gridpane;
 
     @FXML
     public void initialize()
     {
         searchButton.setDisable(true);
+        searchButton.setDefaultButton(true);
+        gridpane.setStyle("-fx-background-image: url('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxsasGQIwQNwjek3F1nSwlfx60g6XpOggnxw5dyQrtCL_0x8IW') ;");
+
     }
 
     @FXML
@@ -23,12 +29,14 @@ public class Controller {
             System.out.println("Result : " + searchText.getText() );
 
             try {
-                Thread.sleep(10000);
+                Thread.sleep(1000);
             }catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             searchText.clear();
+            searchButton.setDisable(true);
+            searchButton.setDefaultButton(true);
         }
 
     @FXML
@@ -38,4 +46,5 @@ public class Controller {
         Boolean disableSearchButton = text.isEmpty() || text.trim().isEmpty();
         searchButton.setDisable(disableSearchButton);
     }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +15,9 @@ public class Main extends Application {
         primaryStage.setTitle("What Do You Search ?");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
-    }
+        primaryStage.setResizable(false);
+
+     }
 
 
     public static void main(String[] args) {
