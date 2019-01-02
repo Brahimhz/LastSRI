2b7ab37447094859701b15846f02a03695afb3cd
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2019-01-02 23:28:50 +0100

    Interface Vers.01 (with Transation & withou List )

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,20 @@
 package sample;
 
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -28,16 +39,58 @@ public class Controller {
         {
             System.out.println("Result : " + searchText.getText() );
 
-            try {
+           /* try {
                 Thread.sleep(1000);
             }catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
 
             searchText.clear();
             searchButton.setDisable(true);
             searchButton.setDefaultButton(true);
+
+            resultTransation();
         }
+
+    private void resultTransation() {
+
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.seconds(1));
+        fadeTransition.setNode(gridpane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+
+        fadeTransition.setOnFinished( (ActionEvent event ) -> {
+             loadResult();
+            });
+
+        fadeTransition.play();
+    }
+
+    private void loadResult()
+    {
+
+
+            try {
+                Parent resultView;
+                resultView= FXMLLoader.load(getClass().getResource("Result.fxml"));
+
+                Scene newScene = new Scene(resultView,1000,600);
+
+                Stage curStage =(Stage) gridpane.getScene().getWindow();
+
+                curStage.setScene(newScene);
+
+                curStage.setTitle("Your Result");
+
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
 
     @FXML
     public void noSpace ()
---
+++ src/sample/Result.java
@@ -0,0 +1,52 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class Result {
+
+    @FXML
+    private TextField searchText;
+    @FXML
+    private Button searchButton ;
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    public void initialize()
+    {
+        searchButton.setDisable(true);
+        searchButton.setDefaultButton(true);
+        borderPane.setStyle("-fx-background-image: url('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxsasGQIwQNwjek3F1nSwlfx60g6XpOggnxw5dyQrtCL_0x8IW') ;");
+
+    }
+
+    @FXML
+    public void onButtonClicked()
+    {
+        System.out.println("Result : " + searchText.getText() );
+
+           /* try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+
+        searchText.clear();
+        searchButton.setDisable(true);
+        searchButton.setDefaultButton(true);
+
+
+    }
+
+    @FXML
+    public void noSpace ()
+    {
+        String text = searchText.getText() ;
+        Boolean disableSearchButton = text.isEmpty() || text.trim().isEmpty();
+        searchButton.setDisable(disableSearchButton);
+    }
+}
